2a33afce3c96eb4f5c9a22cfedeb2327beffc917
Simon <62551180+simonagleev@users.noreply.github.com>
2022-12-21 19:06:19 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/openai', require('./routes/openaiRoutes'))
+
+app.listen(port, () => {
+     console.log(`Serever strated on port ${port}`)
+});
